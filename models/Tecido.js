00fd4d2f6f8e580f49f69e59b048619ba25c6378
Eduardo Lovo <eduardo.llovo@gmail.com>
2023-07-13 09:01:35 -0300

    create parametro estoque para tecido

--- models/Tecido.js
+++ models/Tecido.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const LencolTecido = mongoose.model("LencolTecido", {
     img: String,
-    quantidade: String,
+    quantidade: Number,
     cor: String,
-    tamanho: String
+    tamanho: String,
+    estoque: String
 });
 
 module.exports = LencolTecido;
